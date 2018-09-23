50a6e1018b709256442c1f00b98051cd29039234
curquiza <clementine.urquizar@gmail.com>
2018-09-23 17:41:14 +0200

    add application

---
+++ 01_dockerfiles/ex02/blog/db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Article.create(author: 'Clem', title: 'Un 1er test', content: 'J\'écris pour la premiere fois dans ce blog.')
+Article.create(author: 'Le Chat', title: 'Miaou', content: 'Miaooooooooooouuuuuu. Mia mia ooouuuu')
