68b5d2fdd0783a40e75f8de440d3e5a1fc1b4797
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-08-11 23:28:51 +0200

    Add strace bonus

---
+++ 02_bonus/strace/include/ft_strace.h
@@ -0,0 +1,12 @@
+#ifndef FT_STRACE_H
+# define FT_STRACE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/ptrace.h>
+
+# include <sys/types.h>	// ?
+# include <sys/wait.h>	// ?
+# include <string.h>	// ?
+
+#endif
